97d008fa5af7ddb867bd6cde0d6837bbaee3198f
unknown <jordje9@hotmail.com>
2019-03-07 16:50:58 +0100

    readme

---
+++ src/main/java/nl/hu/v1dp/bienos/App.java
@@ -0,0 +1,13 @@
+package nl.hu.v1dp.bienos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
